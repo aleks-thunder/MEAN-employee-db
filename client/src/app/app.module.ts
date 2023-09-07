a3916ca841a364e51353f22c4b8721a277eb9b1a
aleks-thunder <aleks.thunder.m@gmail.com>
2023-09-07 15:41:24 +0300

    client setup

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { EmployeeFormComponent } from './employees/employee-form/employee-form.component';
+
+@NgModule({
+  declarations: [AppComponent, EmployeesComponent, EmployeeFormComponent],
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
