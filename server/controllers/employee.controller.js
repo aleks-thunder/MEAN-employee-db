bc744c1d467bd0ec7f98bb6145c432c4103ea564
aleks-thunder <aleks.thunder.m@gmail.com>
2023-09-12 12:28:50 +0300

    create basic controller + model

---
+++ server/controllers/employee.controller.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const router = express.Router();
+const ObjectId = require("mongoose").Types.ObjectId;
+
+const Employee = require("../models/employee.model");
+
+router.get("/", (req, res) => {
+  Employee.find()
+    .then((data) => res.send(data))
+    .catch((err) => console.log(err));
+});
+
+router.get("/:id", (req, res) => {
+  if (ObjectId.isValid(req.params.id) == false) {
+    res.status(400).json({ error: "Giver id is not valid " });
+  } else {
+    Employee.findById(req.params.id)
+      .then((data) =>
+        data
+          ? res.send(data)
+          : res
+              .status(404)
+              .json({ error: "No record with given _id" + req.params.id })
+      )
+      .catch((err) => console.log(err));
+  }
+});
+
+router.post("/", (req, res) => {
+  Employee.create(req.body)
+    .then((data) => res.status(201).json(data))
+    .catch((err) => console.log(err));
+});
+
+module.exports = router;
--- server/index.js
+++ server/index.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const bodyParses = require("body-parser");
 
-const connectDb = require("./db.js");
+const connectDb = require("./db");
+const employeeRouter = require("./controllers/employee.controller");
 
 const app = express();
 
 //middleware
 app.use(bodyParses.json());
+app.use("/api/employees", employeeRouter);
 
 connectDb()
   .then(() => {
     console.log("Database connected");
-    app.listen(3000, () => console.log("Server started"));
+    app.listen(3000, () => console.log("Server started at 3000"));
   })
   .catch((error) => console.log(error));
